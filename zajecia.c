75e06090a0bb873ca5f3f68244fcbf741d2e03e2
Rafał Prokopowicz <rprokopowicz@linux-p110-10.inf.ug.edu.pl>
2017-10-22 11:28:15 +0200

    arytmetyka

--- zajecia.c
+++ zajecia.c
@@ -2,6 +2,20 @@
 #include <math.h>
 
 int main() {
-  printf("%d\n", 2+2);
+  printf("%d\n", 2+2); //dodawanie
+  printf("%d\n", 2-2); //odejmowanie
+  printf("%d\n", 2*2); //mnożenie
+  printf("%d\n", 2/2); //dzielenie
+  printf("%d\n", 2|2); //
+  printf("%d\n", 2&2); //
+  printf("%d\n", 2||2); //
+  printf("%d\n", 2&&2); //
+  printf("%d\n", 2>=2); //
+  printf("%d\n", 2<2); //
+  printf("%d\n", 2>2); //
+  printf("%d\n", 2==2); //
+  //printf("%d\n", 2=>2); //
+  printf("%d\n", 2>>2); //
+  printf("%d\n", 2<<2); //
   return 0;
 }
