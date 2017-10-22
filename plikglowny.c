f0d9ef9fc54eccbd73085971625358156664f752
Rafał Prokopowicz <rprokopowicz@linux-p110-10.inf.ug.edu.pl>
2017-10-22 12:04:34 +0200

    cos tam

---
+++ plikglowny.c
@@ -0,0 +1,4 @@
+int main() {
+  hej();
+  return 0;
+}
--- zajecia.c
+++ zajecia.c
@@ -10,12 +10,16 @@ int main() {
   printf("%d\n", 2&2); //
   printf("%d\n", 2||2); //
   printf("%d\n", 2&&2); //
-  printf("%d\n", 2>=2); //
-  printf("%d\n", 2<2); //
-  printf("%d\n", 2>2); //
-  printf("%d\n", 2==2); //
-  //printf("%d\n", 2=>2); //
-  printf("%d\n", 2>>2); //
-  printf("%d\n", 2<<2); //
+
+  //0 - fałsz
+  //1 - prawda
+  printf("%d\n", 2>=2); //wieksze równe
+  printf("%d\n", 2<2); //mniejsze
+  printf("%d\n", 2>2); //wieksze
+  printf("%d\n", 2==2); //równe
+  printf("%d\n", 2>=2); //wieksze równe
+
+  printf("%d\n", 2>>2); //przesuniecie bitowe w prawo
+  printf("%d\n", 2<<2); //przesuniecie bitowe w lewo
   return 0;
 }
